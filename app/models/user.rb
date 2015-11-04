6dcb0eb8319b5117f5abd77c2ab5de1d5b298d1f
Nicole Hall <nicolealexandrahall@gmail.com>
2015-11-04 16:14:55 -0700

    added authorization and admin role.  Test passes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :songs
+
+  validates :username, presence: true,
+                      uniqueness: true
+
+  enum role: %w(default admin)
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104155323) do
+ActiveRecord::Schema.define(version: 20151104212957) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20151104155323) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "role",            default: 0
   end
 
 end
